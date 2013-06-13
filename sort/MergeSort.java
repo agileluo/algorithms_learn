370e7f73ec910c785d17c7ae9fd9156675710bb8
agileluo <universes@yahoo.cn>
2013-06-13 22:02:52 +0800

    add mergetSort class

---
+++ sort/MergeSort.java
@@ -0,0 +1,43 @@
+
+/**
+ * 自顶向下归并排序
+ * @author agileluo
+ *
+ */
+public class MergeSort {
+	public static void main(String[] args) {
+		String[] a = In.readStrings();
+		SortExample.show(a);
+		MergeSort.sort(a);
+		assert SortExample.isSorted(a);
+		SortExample.show(a);
+	}
+	private static Comparable[] aux;
+	
+	public static void sort(String[] a) {
+		aux = new Comparable[a.length];
+		sort(a, 0, a.length-1);
+	}
+
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if( hi <= lo ) return;
+		int mid = lo + (hi - lo)/2;
+		sort(a, lo, mid);
+		sort(a, mid + 1, hi);
+		merge(a, lo, mid, hi);
+	}
+
+	private static void merge(Comparable[] a, int lo, int mid, int hi) {
+		for(int i = lo; i <= hi; i++){
+			aux[i] = a[i];
+		}
+		int loIndex = lo;
+		int hiIndex = mid + 1;
+		for(int k = lo; k <= hi; k++){
+			if( loIndex > mid ) a[k] = aux[hiIndex++];
+			else if (hiIndex > hi) a[k] = aux[loIndex++];
+			else if (SortExample.less(aux[hiIndex], aux[loIndex])) a[k] = aux[hiIndex++];
+			else a[k] = aux[loIndex++];
+		}
+	}
+}
--- src/SortExample.java
+++ src/SortExample.java
@@ -19,13 +19,13 @@ public class SortExample {
 	public static void exch(Comparable[] a , int i, int j){
 		Comparable t = a[i]; a[i] = a[j]; a[j] = t;
 	}
-	private static void show(Comparable[] a){
+	public static void show(Comparable[] a){
 		for(Object o : a){
 			StdOut.print(o + " ");
 		}
 		StdOut.println();
 	}
-	private static boolean isSorted(Comparable[] a){
+	public static boolean isSorted(Comparable[] a){
 		for(int i = 1; i < a.length; i++){
 			if(less(a[i], a[i-1])) return false;
 		}
